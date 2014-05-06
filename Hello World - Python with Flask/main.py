baea5c8a67cff30e2767173048b210e5ec3456b6
root <root@fedora.(none)>
2014-05-05 21:21:04 -0400

    changed text

--- Hello World - Python with Flask/main.py
+++ Hello World - Python with Flask/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello, Udacity!'
+    return 'Hello! Udacity!'
 
 
 @app.errorhandler(404)
